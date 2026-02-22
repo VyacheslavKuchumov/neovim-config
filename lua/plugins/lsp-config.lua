2cb0a5d6b6fb1958cfc4ee2f42e31da019e68e83
VyacheslavKuchumov <87779022+VyacheslavKuchumov@users.noreply.github.com>
2026-02-22 20:18:43 +0500

    improved lsp-config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -6,38 +6,43 @@ return {
       require("mason").setup()
     end,
   },
+
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
     opts = {
-      auto_install = true,
+      automatic_installation = true,
     },
   },
+
   {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      
-      local lspconfig = require("lspconfig")
-      
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      local servers = {
+        "ts_ls",      -- TypeScript
+        "solargraph", -- Ruby
+        "html",       -- HTML
+        "lua_ls",     -- Lua
+        "pyright",    -- Python
+        "gopls",      -- Go
+        "clangd",     -- C/C++
+      }
+
+      for _, server in ipairs(servers) do
+        vim.lsp.config(server, {
+          capabilities = capabilities,
+        })
+        vim.lsp.enable(server)
+      end
+
+      -- Keymaps
+      vim.keymap.set("n", "K", vim.lsp.buf.hover)
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references)
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
     end,
   },
 }
